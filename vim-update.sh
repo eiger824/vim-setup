e250b7b310dbf7e135758161e224e932e6caa516
Santiago Pagola <santipagola@gmail.com>
2019-09-08 21:42:22 +0200

    add option parsing for updater

--- vim-update.sh
+++ vim-update.sh
@@ -15,6 +15,26 @@ OPTIONS:
 EOF
 }
 
+# Parse options
+shortopts="h"
+longopts="--long help"
+options=$(getopt -o ${shortopts} ${longopts} -- "$@")
+[ $? -eq 0 ] ||
+{
+    usage
+    exit 1
+}
+eval set -- "$options"
+while true; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+    esac
+    shift
+done
+
 # For most plugins
 vim +PluginUpdate +qall
 # For YCM
